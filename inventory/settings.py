63047c85c0bcd139ecbb38975404f7d45583dce7
nazmulmilon <milonsarker73@gmail.com>
2021-12-05 12:06:42 +0600

    template directory change

--- inventory/settings.py
+++ inventory/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     # os.path.join(BASE_DIR, 'static')
-    BASE_DIR/'static'
+    'static'
 ]
 
 # Default primary key field type
