4125e37774b56f575d5d718e71aac6887a12d8b8
Hachibkhan <hachib.khan19@gmail.com>
2021-12-13 19:56:20 +0600

    creating product app

--- inventory/settings.py
+++ inventory/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pos_app',
+    #App
+    'pos_app',    
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
--- pos_app/urls.py
+++ pos_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='pos_app'
 urlpatterns=[
     path('', views.home, name='home'),
 
